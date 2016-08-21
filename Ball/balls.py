72eb81efd3b90f3d32f2308c8d9cfeae42bd558b
poikNplop <henry@teabreak.fsnet.co.uk>
2016-08-21 08:55:08 +0100

    Ball Argument Added

--- Ball/balls.py
+++ Ball/balls.py
@@ -1,6 +1,7 @@
 img = 'image'
 data = 'data'
 name = 'name'
+slot = 'slot'
 
 def makedata(health, speed, **args):
     odata = args
@@ -9,18 +10,22 @@ def makedata(health, speed, **args):
 balls = [
     {name:'Little Bad Ball',
      img:'Ball00.png',
-     data:makedata(20,10)
+     data:makedata(20,10),
+     slot:[4,[(50,100,0),(100,50,90),(50,0,180),(0,50,270)]]
      },{
      name:'Armoured Blues',
      img:'Ball01.png',
-     data:makedata(25,10,armour=2)
+     data:makedata(25,10,armour=2),
+     slot:[4,[(50,100,0),(100,50,90),(50,0,180),(0,50,270)]]
      },{
      name:'Square Plus 10',
      img:'Ball.png',
-     data:makedata(30,10,bonusdamage=[10,0,0])
+     data:makedata(30,10,bonusdamage=[10,0,0]),
+     slot:[4,[(50,100,0),(100,50,90),(50,0,180),(0,50,270)]]
      }#,{
 #     name:'',
 #     img:'Ball.png',
-#     data:makedata(20,10)
+#     data:makedata(20,10),
+#     slot:[4,[(50,100,0),(100,50,90),(50,0,180),(0,50,270)]]
 #     }
      ]
